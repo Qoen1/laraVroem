ccb959699614ce1749a8eac578de907935969b35
koen verstappen <koenverstappen2003@gmail.com>
2023-06-21 00:15:38 +0200

    validation messages

--- resources/views/refuel/create.blade.php
+++ resources/views/refuel/create.blade.php
@@ -3,7 +3,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 d-flex justify-content-center">
-                    <h1 class="display-1">Add Drive</h1>
+                    <h1 class="display-1">Add Refuel</h1>
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session()->get('success') }}
+                        </div>
+                        <br>
+                    @endif
+                    @if($errors->any())
+                        <div class="alert alert-danger ">
+                            {{ $errors->all()[0] }}
+                        </div>
+                        <br>
+                    @endif
                 </div>
             </div>
         </div>
