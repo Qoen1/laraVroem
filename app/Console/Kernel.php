490f30a1e568af393c320bc30fb6251781c38d8b
Qoen1 <k.verstappen1@student.avans.nl>
2023-06-26 21:21:16 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,9 +15,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
          $schedule->call(function(){
-             Log::info('testing scheduling...');
-             exec('mysqldump -u '. env('DB_USERNAME').' -p'.env('DB_PASSWORD').' --databases '.env('DB_DATABASE').' > /home/fikkie/backups/'.Carbon::now()->format('d-m-Y').'.sql');
-         })->everyMinute();
+             Log::info('updating database...');
+             exec('mysqldump -u '. env('DB_USERNAME').' -p'.env('DB_PASSWORD').' --databases '.env('DB_DATABASE').' > /home/fikkie/backups/'.Carbon::now()->format('Y-m-d H:i').'.sql');
+         })->weeklyOn(1, '12:00')->timezone('Europe/Amsterdam');
     }
 
     /**
