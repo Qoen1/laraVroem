554a10e7efd4aba47486fd3fb1c604d25db9952e
Qoen1 <k.verstappen1@student.avans.nl>
2023-06-28 00:07:52 +0200

    extracted weekly backup to job file and added env row for more compatibility

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\WeeklyBackup;
 use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -14,10 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->call(function(){
-             Log::info('updating database...');
-             exec('mysqldump -u '. env('DB_USERNAME').' -p'.env('DB_PASSWORD').' --databases '.env('DB_DATABASE').' > /home/fikkie/backups/'.Carbon::now()->format('Y-m-d H:i').'.sql');
-         })->weeklyOn(1, '12:00')->timezone('Europe/Amsterdam');
+         $schedule->job(WeeklyBackup::class)->weeklyOn(1, '12:00')->timezone('Europe/Amsterdam');
     }
 
     /**
