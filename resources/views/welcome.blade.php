904a956fd977bddb2d51074227975b79f08b11a4
koen verstappen <koenverstappen2003@gmail.com>
2023-12-09 15:51:37 +0100

    bugfixes on production :)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 d-flex justify-content-center">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="display-1">Welcome to the car management system</h1>
+		    <h1 class="display-1">Welcome to the car management system</h1>
+		    <p> the current time is: {{Carbon\Carbon::now();}} </p>
                 </div>
             </div>
         </div>
