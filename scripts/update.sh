d4bb8e3d2026d5d8363ddad7f647c61baf0924da
KoenVerstappen <k.verstappen1@student.avans.nl>
2023-12-09 17:21:34 +0100

    updated update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,12 +3,14 @@
 #the second argument is the username with wich to login to the database
 #the third argument is the password with wich to login to the database
 #the fourth argument is the name of the database to backup
-cd /var/www/laraVroem
+
+# go to project root
+cd ..
+
+# disable site
 php artisan down
-# make backup
-git pull
-php artisan migrate --force
-php artisan up
+
+# backup database
 NAME="update on "
 DATE=$(date +"%Y-%m-%d %T")
 PATH=$1
@@ -16,3 +18,26 @@ EXTENTION=.sql
 FILENAME="$PATH$NAME$DATE$EXTENTION"
 echo "saved database backup to: $FILENAME"
 /usr/bin/mysqldump -u "$2" -p"$3" --databases "$4" > "$FILENAME"
+
+#pull latest version from git
+git pull
+
+# update database
+php artisan migrate --force
+
+# update composer
+composer update
+
+# update npm
+npm install
+npm run build
+
+# clear cache
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan optimize
+
+# enable website
+php artisan up
